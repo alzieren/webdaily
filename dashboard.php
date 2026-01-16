b354c54035b0898726081e8c10619cce951abf9a
alzieren <alzieren@gmail.com>
2026-01-16 22:23:42 +0700

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,11 +1,32 @@
-<?php
-//query untuk mengambil data article
-$sql1 = "SELECT * FROM article ORDER BY tanggal DESC";
-$hasil1 = $conn->query($sql1);
 
-//menghitung jumlah baris data article
-$jumlah_article = $hasil1->num_rows; 
+
+<?php
+
+$user = $_SESSION['username'];
+$data_user = $conn->query("SELECT * FROM user WHERE username='$user'")->fetch_assoc();
+
+// article
+$sql1 = "SELECT * FROM article";
+$hasil1 = $conn->query($sql1);
+$jumlah_article = $hasil1->num_rows;
+
+// gallery
+$sql2 = "SELECT * FROM gallery";
+$hasil2 = $conn->query($sql2);
+$jumlah_gallery = $hasil2->num_rows;
 ?>
+
+
+
+<div class="text-center my-4">
+    <p class="fs-3">Selamat Datang,</p>
+
+    <h5 class="text-danger fw-bold fs-3"><?= $data_user['username'] ?></h5>
+    <img src="img/<?= $data_user['foto'] ?>" class="img-fluid rounded-circle mb-2" style="width:160px;height:160px;" alt="Foto Profil">
+    
+</div>
+
+
 <div class="row row-cols-1 row-cols-md-4 g-4 justify-content-center pt-4">
     <div class="col">
         <div class="card border border-danger mb-3 shadow" style="max-width: 18rem;">
@@ -29,7 +50,10 @@ $jumlah_article = $hasil1->num_rows;
                         <h5 class="card-title"><i class="bi bi-camera"></i> Gallery</h5> 
                     </div>
                     <div class="p-3">
-                        <span class="badge rounded-pill text-bg-danger fs-2">0</span>
+                        <span class="badge rounded-pill text-bg-danger fs-2">
+    <?php echo $jumlah_gallery; ?>
+</span>
+
                     </div> 
                 </div>
             </div>
